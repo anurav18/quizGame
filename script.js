8d94f8d0978515ca4f32ad3000405134bdcb51eb
Anu Rav <anusharavva@gmail.com>
2020-09-26 21:33:06 -0700

    Initial display of questions without styling

--- script.js
+++ script.js
@@ -10,25 +10,52 @@ function startTimer(duration, display) {
 
         display.textContent = minutes + ":" + seconds;
         timer = timer - 1;
-console.log(timer);
+// console.log(timer);
 if(timer == -1)
 {
     clearInterval(interval);
-    alert("The quiz has been ended");
+    // alert("The quiz has been ended");
 }
     }, 1000);
 }
 
-// window.onload = function () {
-//     var quizDuration = 60 * 0.25,
-//         timeDisplay = document.querySelector('#time');
-//     startTimer(quizDuration, timeDisplay);
-// };
+
+let questions = [{
+
+    question:"How are you?",
+    options:["good","bad","not atol good"],
+    answer:"good"
+},
+
+{
+    question:"How are you anu?",
+    options:["good","bad","not atol good"],
+    answer:"bad"
+}
+];
 
 var startBtnEl = document.getElementById("start-quiz");
 var quizDescEl = document.querySelector(".quiz-description");
-var quizDuration = 60 * 0.25,
+var quizDuration = 60 * 0.25;
+var quizOptEl = document.querySelector(".quiz-options");
+var quizQueEl = document.querySelector(".quiz-question");
 timeDisplay = document.querySelector('#time');
+var optionsBtnEl = document.querySelector(".options-class");
+
+quizOptEl.addEventListener("click",function(event)
+{
+event.preventDefault();
+alert(event.target.textContent);
+alert(event.target.getAttribute('index-value'));
+var ind = event.target.getAttribute('index-value');
+if(event.target.textContent === questions[ind].answer)
+{
+ alert('hurray answer matched');
+}
+else{
+    alert('answer not matched');
+}
+});
 
 
 
@@ -38,6 +65,35 @@ startBtnEl.addEventListener("click", function(){
 
     quizDescEl.remove();
     startBtnEl.remove();
-    startTimer(quizDuration, timeDisplay);
+    startTimer(quizDuration, timeDisplay); 
+    var i = 0;
+    quizQueEl.textContent = questions[i].question;
+    for(var j= 0 ; j <3 ;j++)
+        {
+
+            //create a button
+            var btnEl = document.createElement("button");
+
+            //set the content for the button
+            btnEl.textContent = questions[i].options[j];
+
+            //create an id attribute
+            btnEl.setAttribute("index-value",i);
+            btnEl.setAttribute("id",j);
+            btnEl.setAttribute("class","options-class");
+
+            //create a margin 10px
+            btnEl.setAttribute("style","margin:10px;");
+
+            var breakEl = document.createElement("br");
+
+            //append it to the div
+            quizOptEl.appendChild(btnEl);
+            quizOptEl.appendChild(breakEl);
+            
+        }
+ 
 });
+
+
 
