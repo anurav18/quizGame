e0ccaaaeaf03101778bce36a75bf2fda45da9470
Anu Rav <anusharavva@gmail.com>
2020-09-26 17:18:25 -0700

    Basic HTML structure using bootstrap classes

---
+++ script.js
@@ -0,0 +1,26 @@
+// function startTimer(duration, display) {
+//     var timer = duration;
+    
+//   const interval =   setInterval(function () {
+//          var minutes = parseInt(timer / 60, 10);
+//         var seconds = parseInt(timer % 60, 10);
+
+//         var minutes = minutes < 10 ? "0" + minutes : minutes;
+//         var seconds = seconds < 10 ? "0" + seconds : seconds;
+
+//         display.textContent = minutes + ":" + seconds;
+//         timer = timer - 1;
+// console.log(timer);
+// if(timer == -1)
+// {
+//     clearInterval(interval);
+//     alert("The quiz has been ended");
+// }
+//     }, 1000);
+// }
+
+// window.onload = function () {
+//     var quizDuration = 60 * 0.25,
+//         timeDisplay = document.querySelector('#time');
+//     startTimer(quizDuration, timeDisplay);
+// };
